1353bb8ca8c4416d850314366eaa2a5b855122c6
fameowner <fameowner99@gmail.com>
2019-07-24 00:49:00 +0300

    Add object color

--- src/ray_handler.c
+++ src/ray_handler.c
@@ -21,22 +21,22 @@ void sphere_ray_intersection(t_union *un, t_object_lst *object, t_vec v, int x, 
     float k3;
     t_sphere *data = (t_sphere *)object->data;
 
-    k1 = vec_dot_product(vec_sub(v, un->camera.position), vec_sub(v, un->camera.position));
-    k2 = 2 * vec_dot_product(vec_sub(un->camera.position, data->center), vec_sub(v, un->camera.position));
-    k3 = vec_dot_product(vec_sub(un->camera.position, data->center), vec_sub(un->camera.position, data->center)) - data->radius * data->radius;
+    k1 = vec_dot_product(v, v);
+    k2 = 2 * vec_dot_product(data->center,v);
+    k3 = vec_dot_product(data->center, data->center) - data->radius * data->radius;
 
     d = k2 * k2 - 4 * k1 * k3;
 
     if (d >= 0)
     {
-        SDL_SetRenderDrawColor(un->sdl.renderer, 169, 169, 169, 0);
+        SDL_SetRenderDrawColor(un->sdl.renderer, object->color.r, object->color.g, object->color.b, 0);
     }
     else
     {
         SDL_SetRenderDrawColor(un->sdl.renderer, 0, 0, 0, 0);
        
     }
-    SDL_RenderDrawPoint(un->sdl.renderer, x, y);
+    SDL_RenderDrawPoint(un->sdl.renderer, x + W_WIDTH / 2, y + W_HEIGHT / 2);
 }
 
 void ray_intersection(t_union *un)
@@ -46,18 +46,18 @@ void ray_intersection(t_union *un)
     int x;
     int y;
 
-    y = -250;
-    while (y < 500)
+    y = -W_HEIGHT / 2;
+    while (y < W_HEIGHT / 2)
     {
-        x = -250;
-        while (x < 500)
+        x = -W_WIDTH / 2;
+        while (x < W_WIDTH / 2)
         {
             head = un->lst;
             while (head)
             {
-                v.x = x * W_WIDTH / 500;
-                v.y = y * W_HEIGHT / 500;
-                v.z = 1;
+                v.x = x;
+                v.y = y;
+                v.z = 500;
                 if (head->type == SPHERE)
                     sphere_ray_intersection(un, head, v, x ,y);
 
